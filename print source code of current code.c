228759ba47302673a55f781ed97387fcad4c8faa
SalmaAlgayar <salmaalgayar47@gmail.com>
2025-09-09 16:41:17 +0300

    Create print source code of current code.c

---
+++ print source code of current code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  FILE *fp;
+  char c;
+  
+  fp = fopen(__FILE__,"r");
+  
+  while ( (c = getc(fp)) != EOF){
+    putchar(c);
+  }
+  
+  fclose(fp);
+}
+
+
+
+
